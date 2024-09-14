98eae8807eb166799738fb4b354169914646fa01
Matias Bazzano <matiasbazzano19@gmail.com>
2024-09-13 21:50:15 -0300

    Update XMLTags.java

--- src/main/java/org/example/data/XMLTags.java
+++ src/main/java/org/example/data/XMLTags.java
@@ -2,7 +2,10 @@ package org.example.data;
 
 public class XMLTags {
     public static final String serviceId = "serviceId";
+    public static final String serviceName = "serviceName";
+    public static final String serviceBalance = "serviceBalance";
+    public static final String statusType = "statusType";
     public static final String expectedServiceName = "expectedServiceName";
     public static final String expectedServiceBalance = "expectedServiceBalance";
-    public static final String sqlStatement = "sqlStatement";
+    public static final String expectedServiceStatus = "expectedServiceStatus";
 }

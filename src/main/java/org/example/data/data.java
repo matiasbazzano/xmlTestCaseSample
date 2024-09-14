77bb41304b5c2673fdf999cbb36a8cb55e875635
Matias Bazzano <matiasbazzano19@gmail.com>
2024-09-13 22:57:58 -0300

    update

--- src/main/java/org/example/data/data.java
+++ src/main/java/org/example/data/data.java
@@ -1,6 +1,6 @@
 package org.example.data;
 
 public class data {
-    public static final String testPath = "src/main/java/org/example/testCases/testCase1.xml";
+    public static final String testPath = "src/main/java/org/example/testCases/testCase4.xml";
     public static final String bd = "jdbc:sqlite:services.db";
 }
